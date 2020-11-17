3d944b0173db975c4f33993946841785629f58fc
JapmanN <japmannagra20@gmail.com>
2020-11-17 17:00:19 -0500

    Make deck of cards

--- Blackjack/src/Deck.java
+++ Blackjack/src/Deck.java
@@ -6,4 +6,13 @@ public class Deck {
     public Deck() {
         this.cards = new ArrayList<String>();
     }
+    
+    // For Each loop to make new deck
+    public void makeDeck() {
+        for(Suit suit : Suit.values()) {
+            for(Value value : Value.values()) {
+                this.cards.add(value.toString() + suit);
+            }
+        }
+    }
 }
